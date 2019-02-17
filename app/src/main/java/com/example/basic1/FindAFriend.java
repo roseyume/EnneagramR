7df85d80900433b139772e691614eb8ae4d66990
rbnoeske@protonmail.com <elKRD,1bP^K&NtV0*sEquJkNkJjEYsIG->
2019-02-16 20:40:46 -0600

    Added friends screen

---
+++ app/src/main/java/com/example/basic1/FindAFriend.java
@@ -0,0 +1,13 @@
+package com.example.basic1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindAFriend extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_afriend);
+    }
+}
